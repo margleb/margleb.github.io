8e7c7dacec14a5cbca180f5ed1b6641085152775
margleb <margleb@yandex.ru>
2017-04-21 13:37:29 +0300

    сommit

---
+++ balconrf/js/common.js
@@ -0,0 +1,42 @@
+$(function() {
+	
+	//magnific popup
+	$("a[href='#callback']").magnificPopup({
+		mainClass: 'my-mfp-zoom-in',
+		removalDelay: 300,
+		type: 'inline',
+	});
+	
+	//magnific popup gallery
+$(".item_gallery").magnificPopup({
+		mainClass: 'mfp-zoom-in',
+		type: 'image',
+		tLoading: '',
+		gallery:{
+		enabled: true,
+		},
+	removalDelay: 300,
+	callbacks: {
+			beforeChange: function() {
+			this.items[0].src = this.items[0].src + '?=' + Math.random(); 
+			},
+			open: function() {
+			$.magnificPopup.instance.next = function() {
+			var self = this;
+			self.wrap.removeClass('mfp-image-loaded');
+			setTimeout(function() { $.magnificPopup.proto.next.call(self); }, 120);
+			}
+			$.magnificPopup.instance.prev = function() {
+			var self = this;
+			self.wrap.removeClass('mfp-image-loaded');
+			setTimeout(function() { $.magnificPopup.proto.prev.call(self); }, 120);
+			}
+		},
+			imageLoadComplete: function() { 
+			var self = this;
+			setTimeout(function() { self.wrap.addClass('mfp-image-loaded'); }, 16);
+			}
+		}
+});
+	
+});
